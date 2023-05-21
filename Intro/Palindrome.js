8e9b5c3e4de9e5b6e6e95b61ff6d0010a3cc9c84
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-05-21 08:49:19 -0400

    Check if string is Palindrome

---
+++ Intro/Palindrome.js
@@ -0,0 +1,3 @@
+// CodeSignal: https://app.codesignal.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ/solutions
+
+const solution = (inputString) => inputString === inputString.split("").reverse().join("");
