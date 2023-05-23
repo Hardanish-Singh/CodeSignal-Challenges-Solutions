ff7af2101fae35bf2123d4fc9f8dbc461353148b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-05-23 07:01:28 -0400

    Shape Area

--- Intro/ShapeArea.js
+++ Intro/ShapeArea.js
@@ -1,9 +1,9 @@
 // CodeSignal: https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ
 
-function solution(n) {
+const solution = (n) => {
         let area = 1;
         for (let i = n; i > 1; i--) {
                 area += (i - 1) * 4;
         }
         return area;
-}
+};
