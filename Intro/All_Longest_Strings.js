a36ab22b48329fc8d9b6b2d43476433722d6b111
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-07-08 13:25:34 -0400

    Add Longest Strings

--- Intro/All_Longest_Strings.js
+++ Intro/All_Longest_Strings.js
@@ -1,7 +1,7 @@
 // CodeSignal: https://app.codesignal.com/arcade/intro/level-3/fzsCQGYbxaEcTr2bL/
 
 const solution = (inputArray) => {
-    let sortedArrayByLength = inputArray.sort( (a, b) => b.length - a.length );
-    let index = sortedArrayByLength.findIndex( arr => arr.length !== sortedArrayByLength[0].length );
-    return sortedArrayByLength.slice( 0, index === -1 ? 1 : index );
-}
+    let sortedArrayByLength = inputArray.sort((a, b) => b.length - a.length);
+    let index = sortedArrayByLength.findIndex((arr) => arr.length !== sortedArrayByLength[0].length);
+    return sortedArrayByLength.slice(0, index === -1 ? 1 : index);
+};
