05aafdcd8352e872d76c46f06b6ae2b2f4386247
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-07-08 13:26:58 -0400

    Make Array Consecutive

--- Intro/Make_Array_Consecutive_2.js
+++ Intro/Make_Array_Consecutive_2.js
@@ -1,12 +1,12 @@
 // CodeSignal: https://app.codesignal.com/arcade/intro/level-2/bq2XnSr5kbHqpHGJC
 
 const solution = (statues) => {
-        statues.sort((a, b) => a - b);
-        let minStatuesNeeded = 0;
-        for (let i = statues[0]; i < statues[statues.length - 1]; i++) {
-                if (!statues.includes(i)) {
-                        minStatuesNeeded++;
-                }
+    statues.sort((a, b) => a - b);
+    let minStatuesNeeded = 0;
+    for (let i = statues[0]; i < statues[statues.length - 1]; i++) {
+        if (!statues.includes(i)) {
+            minStatuesNeeded++;
         }
-        return minStatuesNeeded;
+    }
+    return minStatuesNeeded;
 };
