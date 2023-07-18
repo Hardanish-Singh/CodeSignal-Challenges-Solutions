cef68e8705c04667a944d0baf7cf9e9dd35f4e22
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2023-07-18 10:51:29 -0400

    Reverse In Parenthesis

---
+++ Intro/ReverseInParentheses.js
@@ -0,0 +1,24 @@
+// CodeSignal: https://app.codesignal.com/arcade/intro/level-3/9DgaPsE2a7M6M2Hu6/
+
+const solution = (inputString) => {
+    let stack = [];
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] === "(") {
+            stack.push(i);
+        } else if (inputString[i] === ")") {
+            let index = stack.pop();
+            let lastIndex = i;
+            while (inputString[lastIndex - 1] === ")") {
+                lastIndex--;
+            }
+            let temp = inputString
+                .slice(index + 1, lastIndex)
+                .split("")
+                .reverse()
+                .join("");
+            inputString = inputString.slice(0, index) + temp + inputString.slice(lastIndex + 1);
+            i = -1;
+        }
+    }
+    return inputString;
+};
