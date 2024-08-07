4a613c3e3be31b2ae8d13f86d244797e21817da1
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-07 15:27:22 -0400

    Adjacent Elements Product

--- Intro/AdjacentElementsProduct.js
+++ Intro/AdjacentElementsProduct.js
@@ -1,6 +1,7 @@
 // CodeSignal: https://app.codesignal.com/arcade/intro/level-2/xzKiBHjhoinnpdh6m/
 
-const solution = (inputArray) => {
+// Solution 1
+var solution = (inputArray) => {
     let maxProduct = Number.NEGATIVE_INFINITY;
     for (let i = 0; i < inputArray.length; i++) {
         const product = inputArray[i] * inputArray[i + 1];
@@ -10,3 +11,12 @@ const solution = (inputArray) => {
     }
     return maxProduct;
 };
+
+// Solution 2
+var solution = (inputArray) =>
+    inputArray.reduce((accumulator, currentItem, currentIndex, inputArray) => {
+        if (currentIndex === inputArray.length - 1) return accumulator;
+        const product = currentItem * inputArray[currentIndex + 1];
+        accumulator = Math.max(accumulator, product);
+        return accumulator;
+    }, Number.NEGATIVE_INFINITY);
