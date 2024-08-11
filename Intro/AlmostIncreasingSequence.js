f05055ae3c2a774f081831ed5b6b5b4a826f3e83
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-11 08:42:54 -0400

    Almost Increasing Sequence

--- Intro/AlmostIncreasingSequence.js
+++ Intro/AlmostIncreasingSequence.js
@@ -1,13 +1,13 @@
 // CodeSignal: https://app.codesignal.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG
 
-const isStrictlyIncreasing = (sequence) => {
-    for (let i = 0; i < sequence.length - 1; i++) {
-        if (sequence[i] >= sequence[i + 1]) {
+const isStrictlyIncreasing = (sequence) =>
+    sequence.reduce((accumulator, _, currentIndex, array) => {
+        if (sequence[currentIndex] >= sequence[currentIndex + 1]) {
+            array.length = 0; // eject early by mutating the iterated array
             return false;
         }
-    }
-    return true;
-};
+        return accumulator;
+    }, true);
 
 // Solution 1
 var solution = (nums) => {
@@ -25,8 +25,9 @@ var solution = (nums) => {
 
 // Solution 2
 var solution = (nums) => {
-    if (isStrictlyIncreasing(nums)) return true;
-
+    if (isStrictlyIncreasing(nums)) {
+        return true;
+    }
     for (let i = 1; i < nums.length; i++) {
         if (nums[i - 1] >= nums[i]) {
             let copy1 = [...nums.slice(0, i - 1), ...nums.slice(i)];
