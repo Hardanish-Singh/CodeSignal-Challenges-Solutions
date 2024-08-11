636a1ec162a1cb4c1a331f903747babcc502155e
Hardanish Singh <hardanishsingh1@gmail.com>
2024-08-11 08:37:54 -0400

    Almost Increasing Sequence

--- Intro/AlmostIncreasingSequence.js
+++ Intro/AlmostIncreasingSequence.js
@@ -23,19 +23,20 @@ var solution = (nums) => {
     return true;
 };
 
-// function solution(nums) {
-//     if (isStrictlyIncreasing(nums)) return true;
+// Solution 2
+var solution = (nums) => {
+    if (isStrictlyIncreasing(nums)) return true;
 
-//     for (let i = 1; i < nums.length; i++) {
-//         if (nums[i - 1] >= nums[i]) {
-//             let copy1 = [...nums.slice(0, i - 1), ...nums.slice(i)];
-//             let copy2 = [...nums.slice(0, i), ...nums.slice(i + 1)];
-//             if (!isStrictlyIncreasing(copy1) && !isStrictlyIncreasing(copy2)) {
-//                 continue;
-//             } else {
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
-// }
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] >= nums[i]) {
+            let copy1 = [...nums.slice(0, i - 1), ...nums.slice(i)];
+            let copy2 = [...nums.slice(0, i), ...nums.slice(i + 1)];
+            if (!isStrictlyIncreasing(copy1) && !isStrictlyIncreasing(copy2)) {
+                continue;
+            } else {
+                return true;
+            }
+        }
+    }
+    return false;
+};
