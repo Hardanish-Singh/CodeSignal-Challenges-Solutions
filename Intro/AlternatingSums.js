34e12a76ef6ce4e16f4760320fe5eea9debc01ea
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-22 11:27:05 -0500

    Alternating Sums

--- Intro/AlternatingSums.js
+++ Intro/AlternatingSums.js
@@ -5,7 +5,8 @@ var solution = (array) => {
     let sum1 = 0;
     let sum2 = 0;
     for (let i = 0; i < array.length; i++) {
-        i % 2 === 0 ? (sum1 += array[i]) : (sum2 += array[i]);
+        if (i % 2 === 0) sum1 += array[i];
+        else sum2 += array[i];
     }
     return [sum1, sum2];
 };
