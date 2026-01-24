4d3a477f664aede05cafa8b16b3d1ada60d41e63
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-24 12:20:23 -0500

    Common Character Count

--- Intro/CommonCharacterCount.js
+++ Intro/CommonCharacterCount.js
@@ -2,12 +2,21 @@
 
 const solution = (s1, s2) => {
     let map1 = {};
-    let map2 = {};
     for (let i = 0; i < s1.length; i++) {
-        s1[i] in map1 ? (map1[s1[i]] += 1) : (map1[s1[i]] = 1);
+        if (s1[i] in map1) {
+            map1[s1[i]] += 1;
+        } else {
+            map1[s1[i]] = 1;
+        }
     }
+
+    let map2 = {};
     for (let i = 0; i < s2.length; i++) {
-        s2[i] in map2 ? (map2[s2[i]] += 1) : (map2[s2[i]] = 1);
+        if (s2[i] in map2) {
+            map2[s2[i]] += 1;
+        } else {
+            map2[s2[i]] = 1;
+        }
     }
 
     let count = 0;
